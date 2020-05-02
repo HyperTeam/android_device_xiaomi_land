FranticCoin/device_xiaomi_land

land: Patch Goodix blobs to load custom libbinder
- Add custom libbinder libbindergx.so
- Replace libbinder.so with custom libbinder in biometrics hal

Co-authored-by: ZeeLog <Danjel.Zammit@gmail.com>
Signed-off-by: JacobDeepu <d2jdeepu@gmail.com>

extract-files.sh
@@ -61,4 +61,12 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
 
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/bin/gx_fpcmd
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/bin/gx_fpd
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/hw/fingerprint.goodix.so
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfp_client.so
+patchelf --replace-needed libbinder.so libbindergx.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfpservice.so
+
 "$MY_DIR"/setup-makefiles.sh

HyperTeam/android_device_xiaomi_land

Signed-off-by: Isaac Chen <tingyi364@gmail.com>

extract-files.sh
@@ -64,4 +64,12 @@ BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
 sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "$BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
 
+DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+patchelf --replace-needed libbinder.so libgxbinder.so "$DEVICE_BLOB_ROOT"/vendor/bin/gx_fpcmd
+patchelf --replace-needed libbinder.so libgxbinder.so "$DEVICE_BLOB_ROOT"/vendor/bin/gx_fpd
+patchelf --replace-needed libbinder.so libgxbinder.so "$DEVICE_BLOB_ROOT"/vendor/lib64/hw/fingerprint.goodix.so
+patchelf --replace-needed libbinder.so libgxbinder.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfp_client.so
+patchelf --replace-needed libbinder.so libgxbinder.so "$DEVICE_BLOB_ROOT"/vendor/lib64/libfpservice.so
+
 "$MY_DIR"/setup-makefiles.sh